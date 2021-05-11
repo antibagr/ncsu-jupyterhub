3e3ebd4b06f449cc0983b81c83b681b1fcb1080c
Anton Bagryanov <antibagr@yandex.ru>
2021-05-11 21:48:27 +0300

    Add nbgrader config directory.

---
+++ nbgrader_configuration/nbgrader_global_config.py
@@ -0,0 +1,5 @@
+c = get_config()
+
+c.CourseDirectory.course_id = "test_course"
+
+c.Exchange.root = "/srv/nbgrader/exchange"
---
+++ nbgrader_configuration/nbgrader_instructor_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+
+
+c.CourseDirectory.root = "/home/admin/test_course"
+
+c.Exchange.path_includes_course = True
