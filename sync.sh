855a3687134a964cd49f6a8ce9ed904b46eef976
Anton Bagryanov <abagraynov@cindicator.com>
2021-08-26 13:25:12 +0300

    [Update] sync.sh with a new CLI tool.

--- sync.sh
+++ sync.sh
@@ -1,6 +1,9 @@
 echo "Starting synchronization ..."
 
-docker exec -it jhub python3 lti_synchronization/app.py
+# source sync.sh
+# source sync.sh --path-out something.json
+
+docker exec -it jhub python3 lti_synchronization/cli.py --path_in lti_synchronization/data/courses.json $*
 
 echo "Restarting container ..."
 
